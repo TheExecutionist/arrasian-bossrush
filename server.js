898b7c3eacbed82583f9a0c6859050cfd21674ad
TheGamerKid0617 <45024097+TheGamerKid0617@users.noreply.github.com>
2021-01-25 13:10:51 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3576,7 +3576,7 @@ const sockets = (() => {
                     socket.camera.x = body.x; socket.camera.y = body.y; socket.camera.fov = 2000;
                     // Mark it as spawned
                     socket.status.hasSpawned = true;
-                    body.sendMessage('You have joined the 4TDM! Welcome to the game.');
+                    body.sendMessage('You have joined FFA! Welcome to the game.');
                     body.sendMessage('You will be invulnerable until you move or shoot.');
                     // Move the client camera
                     socket.talk('c', socket.camera.x, socket.camera.y, socket.camera.fov);
